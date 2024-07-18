c9f00879db83b0deb58c49c0e2adb14341a3f0bf
Mohau Mushi <david1mushi1@gmail.com>
2024-07-18 13:13:13 +0200

    Created fetchProducts method in Alpine.js data store to fetch data from the api

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import '../style.css'
-import Alpine from 'alpinejs'
+import "../style.css";
+import Alpine from "alpinejs";
+import { fetchSingleProduct } from "./data/getProducts";
 
+window.Alpine = Alpine;
 
-window.Alpine = Alpine
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    products: [],
+    async fetchProducts() {
+      const result = await fetchSingleProduct();
+      if (result.response) {
+        this.products = result.response;
+      } else {
+        console.error("Error fetching products:", result.error);
+      }
+    },
+  }));
+});
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('',)
-})
-
-
-Alpine.start()
+Alpine.start();
