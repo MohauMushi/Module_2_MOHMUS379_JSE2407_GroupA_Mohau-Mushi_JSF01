2740863a94edd9df5a71b6ff7d17b7fe7043e5c8
Mohau Mushi <david1mushi1@gmail.com>
2024-07-18 20:55:02 +0200

    Implemented error handling for product data fetching

--- src/main.js
+++ src/main.js
@@ -7,23 +7,35 @@ window.Alpine = Alpine;
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     products: [],
-	modalOpen: false,
-	selectedProduct: {},
+    modalOpen: false,
+    selectedProduct: {},
+    loading: true,
+    error: null,
+    errorMessage: "Data fetching failed, please check your network connection",
     async fetchProducts() {
-      const result = await fetchSingleProduct();
-      if (result.response) {
-        this.products = result.response;
-      } else {
-        console.error("Error fetching products:", result.error);
+      this.loading = true;
+      this.error = null;
+      try {
+        const result = await fetchSingleProduct();
+        if (result.response) {
+          this.products = result.response;
+        } else {
+          throw new Error(this.errorMessage);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        this.error = error.message;
+      } finally {
+        this.loading = false;
       }
     },
-	openModal(product) {
-		this.selectedProduct = product;
-		this.modalOpen = true;
-	  },
-	  closeModal() {
-		this.modalOpen = false;
-	  }
+    openModal(product) {
+      this.selectedProduct = product;
+      this.modalOpen = true;
+    },
+    closeModal() {
+      this.modalOpen = false;
+    }
   }));
 });
 
